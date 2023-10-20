207a8af0cd563304ba10d5246c10f7c3104ada97
Katta <manibhargavi.katta@fiserv.com>
2023-10-20 15:59:17 -0400

    commit

--- src/main/java/com/xpanxion/assignments/student/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/student/JavaOne.java
@@ -1,11 +1,13 @@
 package com.xpanxion.assignments.student;
 
+import java.util.Scanner;
+
 public class JavaOne {
 
     //
     // Constructors
     //
-
+    public Scanner sc = new Scanner(System.in);
     public JavaOne() {}
 
     //
@@ -13,23 +15,74 @@ public class JavaOne {
     //
 
     public void ex1() {
-        System.out.println("Student 1: ex1.");
+        System.out.println("Enter your name: ");
+        System.out.println(sc.nextLine().toUpperCase());
     }
 
     public void ex2() {
-        System.out.println("Student 1: ex2.");
+        System.out.println("Enter a String: ");
+        String string = sc.nextLine();
+        int count = 0;
+
+        for(int i = 0; i<string.length(); i++){
+            if(Character.isUpperCase(string.charAt(i))) count++;
+        }
+        System.out.println("Number of uppercase letters: " + count);
     }
 
     public void ex3() {
-        System.out.println("Student 1: ex3.");
+        System.out.println("Enter a String: ");
+        String[] words = sc.nextLine().split(" ");
+        boolean capital = true;
+
+        for(int i = 0; i<words.length; i++) {
+            if (capital) {
+                words[i] = words[i].toUpperCase();
+                capital = false;
+            } else {
+                words[i] = words[i].toLowerCase();
+                capital = true;
+            }
+
+            System.out.print(words[i] + " ");
+        }
     }
 
     public void ex4() {
-        System.out.println("Student 1: ex4.");
+        System.out.println("Input a an string: ");
+        String word = sc.nextLine();
+        boolean isPalindrome = true;
+        int rightPointer = word.length()-1;
+
+        for (int leftPointer = 0; leftPointer < word.length()/2; leftPointer++){
+            if(word.charAt(leftPointer)!=word.charAt(rightPointer)){
+                isPalindrome = false;
+                break;
+            }
+            rightPointer--;
+        }
+
+        if(isPalindrome) System.out.println("YES");
+        else System.out.println("NO");
     }
 
     public void ex5() {
-        System.out.println("Student 1: ex5.");
+        String string = "";
+        int vowelsCount = 0;
+        int consonantsCount = 0;
+        do{
+            System.out.println("Enter a string: ");
+            string = sc.nextLine();
+            String[] words = string.
+
+            for(int i = 0; i<string.length(); i++){
+                if(string.charAt(i) == ('A' || 'E' || 'I' || 'O' || 'U' || 'a' || 'e' || 'i' || 'o' || 'u')) vowelsCount++;
+                else
+
+
+            }
+
+        } while(!string.equals("quit"));
     }
 
     public void ex6() {
